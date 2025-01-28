8ccd5cd1577d274cde74acdb1e1bf3beceef7ea3
ShallowRed <lucaspoulain2@gmail.com>
2025-01-28 16:05:08 +0100

    Ajout du chargement d'icones sans appel réseau

---
+++ client/icon-collections.ts
@@ -0,0 +1,4 @@
+import type { IconifyJSON } from '@iconify/vue'
+
+const collections: IconifyJSON[] = [{ prefix: 'ri', icons: { 'arrow-right-line': { body: '<path fill="currentColor" d="m16.172 11l-5.364-5.364l1.414-1.414L20 12l-7.778 7.778l-1.414-1.414L16.172 13H4v-2z"/>' } }, width: 24, height: 24 }]
+export default collections
---
+++ scripts/icons.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { icons as riCollection } from '@iconify-json/ri'
+
+/**
+ * Liste de nom d’icônes **sans** le préfixe de la collection Remix Icons qui sont utilisées dans l’application
+ * @type {string[]}
+ */
+const riIconNames = [
+  'arrow-right-line',
+]
+
+/**
+ * Liste de tuples [collectionDIcônes, tableauDeNomsDIcônesUtiliséesDansLApplication]
+ * @type {[import('@iconify/vue').IconifyJSON, string[]][]}
+ */
+export const collectionsToFilter = [
+  [riCollection, riIconNames],
+]
