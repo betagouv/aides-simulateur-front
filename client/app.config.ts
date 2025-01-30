2f9b61502de3162a5a40dcf31e3f11b4c14648e6
ShallowRed <lucaspoulain2@gmail.com>
2025-01-30 18:57:44 +0100

    Extrait les props des header et footer vers l'app config

---
+++ client/app.config.ts
@@ -0,0 +1,30 @@
+export default defineAppConfig({
+  headerProps: {
+    serviceTitle: 'aides simplifiées',
+    serviceDescription: 'La bonne aide, au bon endroit, au bon moment',
+    logoText: ['République', 'Française'],
+    quickLinks: [
+      {
+        label: 'À propos',
+        to: '/content/apropos',
+        icon: { name: 'ri-flag-line', ssr: true },
+      },
+    ],
+  },
+  footerProps: {
+    logoText: ['République', 'Française'],
+    licenceText: 'Licence ouverte 2.0',
+    licenceTo: 'https://www.etalab.gouv.fr/licence-ouverte-open-licence',
+    licenceName: 'Licence ouverte 2.0',
+    a11yCompliance: 'partiellement conforme',
+    descText: 'Description du service',
+    operatorImgAlt: 'Logo de l\'opérateur',
+    operatorLinkText: 'Nom de l\'opérateur',
+    operatorTo: 'https://www.interieur.gouv.fr',
+    homeLink: '/',
+    legalLink: '/content/mentions-legales',
+    personalDataLink: '/content/donnees-personnelles',
+    cookiesLink: '/content/cookies',
+    a11yComplianceLink: '/content/accessibilite',
+  }
+})
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,22 +1,5 @@
 <script setup lang="ts">
-import type { DsfrFooterProps } from '@gouvminint/vue-dsfr'
-
-const serviceTitle = 'Service'
-const serviceDescription = 'Description du service'
-const logoText = ['Ministère', 'de l’Intérieur', 'et des Outre-Mer']
-
-const quickLinks = [
-  {
-    label: 'Accueil',
-    to: '/',
-    icon: { name: 'ri-home-2-line', ssr: true },
-  },
-  {
-    label: 'À propos',
-    to: '/content/apropos',
-    icon: { name: 'ri-flag-line', ssr: true },
-  },
-]
+const { headerProps, footerProps } = useAppConfig()
 
 const skipLinks = [
   {
@@ -28,32 +11,13 @@ const skipLinks = [
     text: 'Accéder au pied de page',
   },
 ]
-
-const footerProps: DsfrFooterProps = {
-  logoText,
-  licenceText: 'Licence ouverte 2.0',
-  licenceTo: 'https://www.etalab.gouv.fr/licence-ouverte-open-licence',
-  licenceName: 'Licence ouverte 2.0',
-  a11yCompliance: 'partiellement conforme',
-  descText: 'Description du service',
-  operatorImgAlt: 'Logo de l’opérateur',
-  operatorLinkText: 'Nom de l’opérateur',
-  operatorTo: 'https://www.interieur.gouv.fr',
-  homeLink: '/',
-  legalLink: '/content/mentions-legales',
-  personalDataLink: '/content/donnees-personnelles',
-  cookiesLink: '/content/cookies',
-  a11yComplianceLink: '/content/accessibilite',
-}
 </script>
 
 <template>
   <DsfrSkipLinks :links="skipLinks" />
   <DsfrHeader
-    :service-title="serviceTitle"
-    :service-description="serviceDescription"
-    :logo-text="logoText"
-    :quick-links="quickLinks"
+    v-if="headerProps"
+    v-bind="headerProps"
   />
   <main
     id="content"
@@ -64,6 +28,7 @@ const footerProps: DsfrFooterProps = {
     <slot />
   </main>
   <DsfrFooter
+    v-if="footerProps"
     id="footer"
     v-bind="footerProps"
     tabindex="0"
