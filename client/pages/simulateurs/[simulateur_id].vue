823ade1b7ab7a6980946e085708a79fd02f1a594
ShallowRed <lucaspoulain2@gmail.com>
2025-02-20 12:55:21 +0100

    Rend le template de page simulateur plus générique

--- client/pages/simulateurs/[simulateur_id].vue
+++ client/pages/simulateurs/[simulateur_id].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import demenagementPictogram from '@/assets/custom-pictograms/demenagement.svg'
+import { simulateurs, type Simulateur } from '@/data/simulateurs'
 
 definePageMeta({
   layout: 'default',
@@ -8,42 +8,48 @@ definePageMeta({
 const route = useRoute()
 const simulateurId = ref(route.params.simulateur_id)
 
-const simulateursProps = [
-  {
-    id: 'demenagement-logement',
-    shortTitle: 'Déménagement & logement',
-    // pictogram: '',
-    title: 'Aides financières au déménagement et au logement',
-  }
-]
-
-const simulateur = computed(() => {
-  return simulateursProps.find(simulateur => simulateur.id === simulateurId.value)
+const simulateur = computed<Simulateur | undefined>(() => {
+  return simulateurs.find(s => s.id === simulateurId.value)
 })
+
+// Handle simulator is not found
+if (!simulateur.value) {
+  throw createError({
+    statusCode: 404,
+    message: 'Simulateur non trouvé'
+  })
+}
+
+// Load pictogram dynamically
+const pictogram = ref<string | undefined>()
+simulateur.value.pictogram()
+  .then((svg) => {
+    pictogram.value = svg.default
+  })
+
 </script>
 
 <template>
-  <section class="fr-background-default--blue-france fr-py-3w fr-py-md-4w">
-    <div class="fr-container">
+  <section v-if="simulateur" class="fr-background-default--blue-france">
+    <div class="fr-container fr-py-3w fr-py-md-4w">
       <div class="fr-grid-row fr-grid-row--gutters">
         <div class="fr-col-12">
-          <DsfrBreadcrumb
-            class="fr-m-0"
-            breadcrumb-id="mon-fil-dariane"
-            :links="[
-              { text: 'Accueil', to: '/' },
-              { text: 'Simulateurs', to: '/simulateurs' },
-              { text: simulateur.title, to: `/simulateurs/${simulateur.id}` },
-            ]"
-          />
+          <DsfrBreadcrumb class="fr-m-0" breadcrumb-id="mon-fil-dariane" :links="[
+            { text: 'Accueil', to: '/' },
+            { text: 'Simulateurs', to: '/simulateurs' },
+            { text: simulateur.title, to: `/simulateurs/${simulateur.id}` },
+          ]" />
         </div>
       </div>
-      <div class="fr-grid-row fr-grid-row--gutters fr-py-2w">
+      <div class="fr-grid-row fr-grid-row--gutters">
         <div class="fr-col-3 fr-col-sm-2 fr-col-md-1">
-          {{ demenagementPictogram }}
+          <DsfrPictogram
+            v-if="pictogram"
+            :svg-path="pictogram"
+          />
         </div>
-        <div class="fr-col-9 fr-col-sm-10 fr-col-md-11">
-          <h1 class="fr-h5 ">
+        <div class="title-container fr-col-9 fr-col-sm-10 fr-col-md-11">
+          <h1 class="fr-h5 fr-m-0">
             {{ simulateur.title }}
           </h1>
         </div>
@@ -60,3 +66,10 @@ const simulateur = computed(() => {
     </div>
   </section>
 </template>
+
+<style scoped lang="scss">
+.title-container {
+  display: flex;
+  align-items: center;
+}
+</style>
