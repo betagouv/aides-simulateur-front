a883985f29c9132613690ef7326e37013a1e211e
ShallowRed <lucaspoulain2@gmail.com>
2025-02-25 19:57:56 +0100

    Création d'un composant pour gérer les styles des sections html

---
+++ client/components/layout/AsSection.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  type?: 'default' | 'user-action' | 'simulation-header' | 'breadcrumb',
+  backgroundColor?: 'default--grey' | 'alt--blue-france' | 'action-high--blue-france'
+}>(), {
+  type: 'default',
+})
+</script>
+
+<template>
+  <section
+    class="as-section"
+    :class="[{
+      'fr-pt-3w fr-pb-1w fr-pt-md-4w fr-pb-md-3v': type === 'breadcrumb',
+      'fr-pt-1w fr-pb-3w fr-pt-md-3v fr-pb-md-4w': type === 'simulation-header',
+      'fr-py-8w fr-py-md-12w': type === 'default',
+      'fr-pt-6w fr-pt-md-8w fr-pb-9W fr-pb-md-12w': type === 'user-action',
+      [ `fr-background-${backgroundColor}`]: Boolean(backgroundColor),
+    }]"
+  >
+    <div class="fr-container">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+</style>
---
+++ client/components/layout/AsSectionSeparator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fr-container">
+    <hr class="as-section-separator">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.as-section-separator {
+  border-top: 1px solid var(--border-open-blue-france);
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+}
+</style>
