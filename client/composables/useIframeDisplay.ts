64a97bc89e9da9a7b1b907688897518f6779175e
ShallowRed <lucaspoulain2@gmail.com>
2025-03-05 11:25:27 +0100

    Met à jour la fonctionnalité de détection d'iframe

--- client/composables/useIframeDisplay.ts
+++ client/composables/useIframeDisplay.ts
@@ -1,42 +1,16 @@
-// client/composables/useIframeDisplay.ts
-import { computed } from 'vue'
-
 export function useIframeDisplay () {
   const route = useRoute()
 
   const isIframe = computed(() => {
-    return route.query.iframe === 'true'
+    return isIframeRoute(route)
   })
 
   const displayOption = computed(() => {
     return route.query['data-display-option'] || ''
   })
 
-  const showHeader = computed(() => {
-    // Si ce n'est pas un iframe, on affiche le header
-    if (!isIframe.value) { return true }
-
-    // Désactiver le header si l'option du iframe est 'no-header'
-    if (displayOption.value === 'no-header') { return false }
-
-    // Afficher le header si c'est explicitement demandé ou par défaut
-    return true
-  })
-
-  const showFooter = computed(() => {
-    // Si ce n'est pas un iframe, on affiche le footer
-    if (!isIframe.value) { return true }
-
-    // Désactiver le footer si l'option du iframe est 'header-only'
-    if (displayOption.value === 'header-only' || displayOption.value === 'no-header') { return false }
-
-    return true
-  })
-
   return {
     isIframe,
     displayOption,
-    showHeader,
-    showFooter,
   }
 }
