eb7d027468d12336ef5fd76e9628f0ec3b5d2855
ShallowRed <lucaspoulain2@gmail.com>
2025-03-06 17:05:13 +0100

    Renome les fichiers de déclarations de type

--- client/types/aides.d.ts
+++ client/types/aides.d.ts
@@ -1,6 +1,15 @@
 declare global {
 
   type TypeAide = 'pret' | 'garantie' | 'caution' | 'periode' | 'une-fois' | 'reduction-impots' | 'aide-materielle' | 'financements'
+
+  interface AideContent {
+    id: string
+    title: string
+    description: string
+    montant: number
+    type: TypeAide
+    resume: string
+  }
 }
 
 export {}
