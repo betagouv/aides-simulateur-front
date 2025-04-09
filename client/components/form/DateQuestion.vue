941020db747de095afb2fe616fcb0fafcc165469
ShallowRed <lucaspoulain2@gmail.com>
2025-04-09 17:08:22 +0200

    Améliore l'a11y du formulaire
    Améliore la navigabilité au clavier du formulaire

--- client/components/form/DateQuestion.vue
+++ client/components/form/DateQuestion.vue
@@ -9,10 +9,10 @@ const model = defineModel<string | undefined>()
 <template>
   <DsfrInputGroup
     v-model="model"
-    class="fr-mb-4w"
     type="date"
     :name="question.id"
     :label="question.title"
     :label-visible="false"
+    :hint="question.description"
   />
 </template>
